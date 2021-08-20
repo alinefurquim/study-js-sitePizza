dc547c73b8fc7fb6b13f433c4bc105e1ebc36564
Aline Furquim <aline1080@gmail.com>
2021-08-19 21:26:15 -0300

    aplicação de querySelector

--- scriptPizza.js
+++ scriptPizza.js
@@ -6,4 +6,4 @@ let pizzaJson = [
     {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description: 'Pizza bem gostosa e bem recheada'},
     {id:6, name:'Moda da casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description: 'Pizza bem gostosa e bem recheada'},
     {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description: 'Pizza bem gostosa e bem recheada'}
-]
+];
