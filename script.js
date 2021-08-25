322ee4a6245acd9bef1483afc2f169c57ae201bf
Aline Furquim <aline1080@gmail.com>
2021-08-25 09:30:01 -0300

    modal structure

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let modalQuantidade = 1;
 const query = (el) => document.querySelector(el);
 const queryAll = (el) => document.querySelectorAll(el);
 
+//Listagem das pizzas
 pizzaJson.map((item, index) => {
     let pizzaItem = query('.models .pizza-item').cloneNode(true);
     
@@ -27,7 +28,7 @@ pizzaJson.map((item, index) => {
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
 
-        query('pizzaInfo--qt').innerHTML = modalQuantidade;
+        query('.pizzaInfo--qt').innerHTML = modalQuantidade;
         query('.pizzaWindowArea').style.opacity = 0;
         query('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
@@ -36,4 +37,35 @@ pizzaJson.map((item, index) => {
     });
 
     query('.pizza-area').append(pizzaItem);
+});
+
+//Eventos do modal
+function closeModal() {
+    query('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        query('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+queryAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+});
+
+query('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQuantidade > 1) {
+        modalQuantidade--;
+        query('.pizzaInfo--qt').innerHTML = modalQuantidade;
+    }    
+});
+
+query('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQuantidade++;
+    query('.pizzaInfo--qt').innerHTML = modalQuantidade;
+});
+
+queryAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+   size.addEventListener('click', (e) => {
+    query('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+   });
 });
